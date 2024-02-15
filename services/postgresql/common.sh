5d9ce8ac4b29abab73d71596cd5dc24394a6f2dc
Francis Kayiwa <kayiwa@pobox.com>
2024-02-15 10:30:35 -0500

    add backup scripts for postgresql
    these scripts are used to backup postgresql and will be part of a cron job

---
+++ services/postgresql/common.sh
@@ -0,0 +1,41 @@
+set -e -E -o pipefail
+
+err_exit() {
+        echo "$0: exit with error on line $1" >&2
+}
+
+trap 'err_exit $LINENO' ERR
+
+[ -f "$HOME/.env.restic" ] && . $HOME/.env.restic
+
+if [ -z "$LOGNAME" ]; then
+        echo LOGNAME must contain the username
+        exit 2
+fi
+
+if [ -z "$RESTIC_REPOSITORY" -a -z "$RESTIC_ARCHIVE_REPOSITORY" ]; then
+        echo one of RESTIC_REPOSITORY and RESTIC_ARCHIVE_REPOSITORY
+        echo must specify the restic repository.
+        exit 2
+fi
+
+if [ -z "$RESTIC_PASSWORD_FILE" ]; then
+        echo RESTIC_PASSWORD_FILE must specify the restic repository password.
+        exit 2
+fi
+
+use_archive_repository() {
+        if [ -n "$RESTIC_ARCHIVE_REPOSITORY" ]; then
+                export RESTIC_REPOSITORY=$RESTIC_ARCHIVE_REPOSITORY
+        fi
+}
+
+use_transient_repository() {
+        if [ -n "$RESTIC_TRANSIENT_REPOSITORY" ]; then
+                export RESTIC_REPOSITORY=$RESTIC_TRANSIENT_REPOSITORY
+        fi
+}
+
+[ -d "log" ] || mkdir log
+
+LOG="log/$(basename $0)-$(date +%Y-%m-%d).log"
---
+++ services/postgresql/push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+. ./common.sh
+
+RCLONE=./rclone
+OPTS='--transfers 32'
+
+if [[ "$RESTIC_ARCHIVE_REPOSITORY" =~ ^/ ]] && \
+        [[ -n "$RCLONE_ARCHIVE_REPOSITORY" ]]
+then
+        $RCLONE $OPTS sync $RESTIC_ARCHIVE_REPOSITORY $RCLONE_ARCHIVE_REPOSITORY \
+                --create-empty-src-dirs
+fi
+
+if [[ "$RESTIC_TRANSIENT_REPOSITORY" =~ ^/ ]] && \
+        [[ "$RESTIC_TRANSIENT_REPOSITORY" != "$RESTIC_ARCHIVE_REPOSITORY" ]] && \
+        [[ -n "$RCLONE_TRANSIENT_REPOSITORY" ]]
+then
+        $RCLONE $OPTS sync $RESTIC_TRANSIENT_REPOSITORY $RCLONE_TRANSIENT_REPOSITORY \
+                --create-empty-src-dirs
+fi
